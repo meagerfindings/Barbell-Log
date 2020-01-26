98aa14c1875f40c381d2135e2aea1283b0fb56fd
Mat Greten <mgreten@gmail.com>
2020-01-26 06:52:35 -0700

    create user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_133606) do
+ActiveRecord::Schema.define(version: 2020_01_26_134106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2020_01_26_133606) do
     t.text "coach_notes"
     t.boolean "completed"
     t.date "due_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.boolean "coach"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
