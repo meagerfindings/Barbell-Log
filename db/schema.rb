f7faa1d815930f076b6f6819009af9c4fc0b50e6
Mat Greten <mgreten@gmail.com>
2020-01-26 17:01:24 -0700

    create movement table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_235804) do
+ActiveRecord::Schema.define(version: 2020_01_27_000028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2020_01_26_235804) do
   create_table "lifts", force: :cascade do |t|
     t.uuid "session_id"
     t.text "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movements", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
