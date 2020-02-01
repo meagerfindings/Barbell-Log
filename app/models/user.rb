c03b66e9dbd6df22b8184119a37dc5d905bff576
Mat Greten <mgreten@gmail.com>
2020-02-01 12:09:43 -0700

    configure devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable
   has_many :sessions
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "application#frontend"
+
   namespace :v1 do
     resources :sessions
     resources :users
