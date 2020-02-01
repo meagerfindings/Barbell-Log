d53736cc48eb1ae27d3a879afb8dda744041b314
Mat Greten <mgreten@gmail.com>
2020-02-01 11:33:51 -0700

    base `yarn build` output within `../public`

---
+++ public/precache-manifest.154b12dc0952eb09f10c955e654a3db1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ff1f7f990e3949049fec123ed216d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e44f4b2e0b685541ea1c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ff8caed4fa20319a54bc",
+    "url": "/static/js/2.ff64ea3a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ff64ea3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44f4b2e0b685541ea1c",
+    "url": "/static/js/main.b0803130.chunk.js"
+  },
+  {
+    "revision": "b12828db4c8e8361c968",
+    "url": "/static/js/runtime-main.0cfccae4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
