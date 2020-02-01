a9444a08c382d085e1d15f9bd624d16d68a5b1f5
Mat Greten <mgreten@gmail.com>
2020-02-01 09:25:25 -0700

    api v1 sessions & users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
+  namespace :v1 do
+    resources :sessions
+    resources :users
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :sessions
-
-    root 'dashboard#index'
 end
